a8ecaeec5ce7aaff1d7d661222eb6c072eb79358
Alexandru Bereghici <abereghici@tripadvisor.com>
2024-01-22 18:45:00 +0200

    update nvim

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,66 @@
+local discipline = require("bereghicidev.discipline")
+
+discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Move visual selections
+keymap.set("v", "K", ":m '>-2<CR>gv=gv", { desc = "Move visual selection up" })
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move visual selection down" })
+
+-- Keep cursor centered
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center the cursor" })
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center the cursor" })
+
+keymap.set("n", "n", "nzzzv", { desc = "Keep search matches in window center" })
+keymap.set("n", "N", "Nzzzv", { desc = "Keep search matches in window center" })
