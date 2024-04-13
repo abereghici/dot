6aaadbb7eae1afc15b6591719a007734c67f864f
Alexandru Bereghici <abereghici@tripadvisor.com>
2024-04-13 12:43:57 +0300

    update treesitter

---
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
