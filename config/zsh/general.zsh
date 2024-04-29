cb9533c687f0a7d14575e5e9156d202536ed3a6c
Alexandru Bereghici <alexandru.brg@gmail.com>
2024-04-29 10:21:01 +0300

    Add user specific environment in path

--- config/zsh/general.zsh
+++ config/zsh/general.zsh
@@ -5,3 +5,9 @@ export LC_ALL="en_US.UTF-8"
 
 # Add /usr/local/bin, the current directory's node_modules/.bin, and the user's .local/bin directory to the system PATH
 export PATH="/usr/local/bin:$PATH:./node_modules/.bin:$HOME/.local/bin"
+
+# Add user bin directory to the system PATH
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]
+then
+  export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
