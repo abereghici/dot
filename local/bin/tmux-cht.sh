d534e99bfae2d41f5227342d1277d27aee339374
Alexandru Bereghici <alexandru.brg@gmail.com>
2024-05-08 16:06:00 +0300

    Add cht.sh script in tmux

---
+++ local/bin/tmux-cht.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+selected=$(cat $HOME/.config/tmux/.tmux-cht-command $HOME/.config/tmux/.tmux-cht-languages | fzf)
+if [[ -z $selected ]]; then
+	exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.config/tmux/.tmux-cht-languages; then
+	query=$(echo $query | tr ' ' '+')
+	tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
