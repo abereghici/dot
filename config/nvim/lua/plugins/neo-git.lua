e51c3745f67a3bbd691841b31cbba46c8a37edf3
Alexandru Bereghici <alexandru.brg@gmail.com>
2024-06-24 20:25:38 +0300

    feat(neogit): update plugins and add neogit
    Several plugins have been updated as shown in the diff of
    `lazy-lock.json`. The commits of `bufferline.nvim`, `catppuccin`,
    `conform.nvim`, `lazy.nvim`, `neogit`, `noice.nvim`, and
    `nvim-treesitter` have been updated.
    
    Additionally, `neogit` has been added to the project with its
    configuration and dependencies. The new file `neo-git.lua` has been
    created for this purpose. The `neogit` plugin can be triggered with
    `<leader>gn` keybinding.

---
+++ config/nvim/lua/plugins/neo-git.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    event = "VeryLazy",
+    config = true,
+    keys = {
+      {
+        "<leader>gn",
+        function()
+          local neogit = require("neogit")
+
+          neogit.open()
+        end,
+        desc = "Neogit",
+        mode = { "n", "v" },
+      },
+    },
+  },
+}
