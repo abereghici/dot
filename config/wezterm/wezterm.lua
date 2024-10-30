1f29afe5729e955530d8c13e186632a664f259e7
Alexandru Bereghici <alexandru.brg@gmail.com>
2024-10-30 15:30:15 +0200

    starship change

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local theme = require("lua/rose-pine").moon
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 config.window_frame = theme.window_frame()
 
