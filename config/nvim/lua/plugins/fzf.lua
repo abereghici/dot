b76caec9b7c7a6f3da81ad4aa7a64e37f550f1c0
Alexandru Bereghici <alexandru.brg@gmail.com>
2024-12-16 16:15:54 +0200

    Remap fzf keys. disable scroll animation

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -18,5 +18,9 @@ return {
         },
       }
     end,
+    keys = {
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    },
   },
 }
